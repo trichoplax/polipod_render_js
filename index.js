b15c0cdf72260e7bb71b45e04727d6ebc8591ac6
trichoplax <xalpohcirt@gmail.com>
2021-02-28 21:31:25 +0000

    Move code from polipod repo into this one to keep variables in scope

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import {squareColour} from '@trichoplax/polipod_colours_js/index.js'
 import {squareAngle} from '@trichoplax/polipod_angles_js/index.js'
 
+const canvas = document.getElementById('polipod_canvas')
+const context = canvas.getContext('2d')
+
+const changeColourCheckbox = document.getElementById('change_colour')
+let changeColourActive = changeColourCheckbox.checked
+changeColourCheckbox.addEventListener('change', () => {
+    changeColourActive = changeColourCheckbox.checked
+})
+
+const changeAngleCheckbox = document.getElementById('change_angle')
+let changeAngleActive = changeAngleCheckbox.checked
+changeAngleCheckbox.addEventListener('change', () => {
+    changeAngleActive = changeAngleCheckbox.checked
+})
+
 let red = 0
 let green = 0
 let blue = 0
 let angle = 0
 
-export const renderLoop = (changeColourActive, changeAngleActive) => {
+export const renderLoop = () => {
     if (changeColourActive) {
         [red, green, blue] = squareColour([red, green, blue])
     }
